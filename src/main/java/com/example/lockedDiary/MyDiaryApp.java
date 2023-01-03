d7511efbfe27d54727dfaae1308fcfe87e28cccd
samuelshola777 <samuelshola14@gmail.com>
2023-01-03 22:33:04 +0100

    simple diary Application

--- src/main/java/com/example/lockedDiary/MyDiaryApp.java
+++ src/main/java/com/example/lockedDiary/MyDiaryApp.java
@@ -4,7 +4,9 @@ import com.example.lockedDiary.controller.DiaryControllerImpl;
 import com.example.lockedDiary.controller.DiaryControllerInterface;
 import com.example.lockedDiary.data.model.DiaryPages;
 import com.example.lockedDiary.exectionThrow.DiaryPageExeption;
+import com.example.lockedDiary.exectionThrow.DiaryServicesExeption;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class MyDiaryApp {
@@ -61,42 +63,48 @@ public static String inputS(){
 
 
 public static void writeMessagePageOnDiary() throws DiaryPageExeption {
-        print("Diary is currently locked bt you can unlocked Diary by creating new password\n" +
-                "Enter a New Password");
-        String password = inputS();
-        print("please enter Event date\n");
-        String date = inputS();
-        print("please enter Event Month \n ");
-    String month = inputS();
+        try {
+            print("Diary is currently locked bt you can unlocked Diary by creating new password\n" +
+                    "Enter a New Password");
+            String password = inputS();
+            print("please enter Event date\n");
+            String date = inputS();
+            print("please enter Event Month \n ");
+            String month = inputS();
 
-    print(" Enter Event Title \n");
-    String title = inputS();
+            print(" Enter Event Title \n");
+            String title = inputS();
 
-    print(" Enter today's  Days EVent");
-    String message = inputS();
-        diary.createDiaryPage(date, month, title, message,password);
-        while (true) {
+            print(" Enter today's  Days EVent");
+            String message = inputS();
+            diary.createDiaryPage(date, month, title, message, password);
+
             print("do you want add to the existing gist?\n\n" +
                     "press 1 to continue the gist  \n" +
                     "press 2 to go the main menu \n" +
                     "press 3 to add to an Existing gist \n");
             int respond = input();
-            if (respond > 3){
-                continue;
-            }else {
-                switch (respond) {
-                    case 1 -> addToMessageBody(date, month, password, message);
-                    case 2 -> diplayMenu() ;
-                    case 3 -> addToAnExistingPage();
-                }
+            switch (respond) {
+                case 1 -> addToMessageBody(date, month, password, message);
+                case 2 -> diplayMenu();
+                case 3 -> addToAnExistingPage();
             }
+        }catch (DiaryPageExeption | IllegalArgumentException | InputMismatchException be){
+            writeMessagePageOnDiary();
         }
 }
+
 public static void addToMessageBody(String date , String month, String password, String message) throws DiaryPageExeption {
-        DiaryPages page = diary.findPageByDate(date, month, password);
-        page.updateMessageBody(message);
+        try {
+            DiaryPages page = diary.findPageByDate(date, month, password);
+            page.updateMessageBody(message);
+        }catch (DiaryPageExeption | IllegalArgumentException | InputMismatchException no){
+            writeMessagePageOnDiary();
+        }
+
 }
 public static void findExistingDiaryPage() throws DiaryPageExeption {
+    try {
     print("please enter password ");
     String password = inputS();
     print("please enter Event date\n");
@@ -104,23 +112,29 @@ public static void findExistingDiaryPage() throws DiaryPageExeption {
     print("please enter Event Month \n ");
     String month = inputS();
 
-print(diary.findPageByDate(date, month, password));
+
+
+    print(diary.findPageByDate(date, month, password));
     while (true) {
         print("do you want add to the existing gist?\n\n" +
                 "press 1 to go back to the main menu \n" +
                 "press 2 to Edith the Existing Diary Page ");
         int respond = input();
-        if (respond > 2){
+        if (respond > 2) {
             continue;
-        }else {
+        } else {
             switch (respond) {
                 case 1 -> diplayMenu();
                 case 2 -> addToAnExistingPage();
             }
         }
     }
+}catch (DiaryPageExeption |IllegalArgumentException| InputMismatchException bone){
+    findExistingDiaryPage();
+}
 }
 public static void addToAnExistingPage() throws DiaryPageExeption {
+try {
 
     print("please enter password ");
     String password = inputS();
@@ -133,71 +147,86 @@ public static void addToAnExistingPage() throws DiaryPageExeption {
     DiaryPages pages = diary.findPageByDate(date, month, password);
     print("""
             >>>>    WELCOME TO THE PAGE EDITING CHANNEL <<<<<<<<< \n\n
-            
+                        
             press 1 to  Update Title   <<<<< \n
-            
+                        
             press 2 to Change Title <<<<<<\n
-            
+                        
             press 3  to Update Message Body <<<<<\n
-            
+                        
             press 4 to delete Massage Body  
             """);
     int response = input();
-    switch (response){
-        case 1 : print("please enter new title  ");
-        String updateTitle = inputS();
+    switch (response) {
+        case 1:
+            print("please enter new title  ");
+            String updateTitle = inputS();
             pages.updateTitle(updateTitle);
             break;
-        case 2 : print("please enter the new title");
-        String newTitle = inputS();
-        pages.changeTitle(newTitle);
-        break;
-        case 3 : print("please enter the updates message body ");
-        String updateMessagebody = inputS();
-        pages.updateMessageBody(updateMessagebody);
-        break;
-        case 4: print("Message deleted ");
-        pages.deleteMessageBody();
-        break;
+        case 2:
+            print("please enter the new title");
+            String newTitle = inputS();
+            pages.changeTitle(newTitle);
+            break;
+        case 3:
+            print("please enter the updates message body ");
+            String updateMessagebody = inputS();
+            pages.updateMessageBody(updateMessagebody);
+            break;
+        case 4:
+            print("Message deleted ");
+            pages.deleteMessageBody();
+            break;
         default:
- }
+    }
     while (true) {
         print("do you want add to the existing gist?\n\n" +
                 "press 1 to go back to the main menu \n" +
                 "press 2 to Edith the Existing Diary Page ");
         int respond = input();
-        if (respond > 2){
+        if (respond > 2) {
             continue;
-        }else {
+        } else {
             switch (respond) {
                 case 1 -> diplayMenu();
                 case 2 -> addToAnExistingPage();
             }
         }
+
     }
+
+}catch (DiaryPageExeption | IllegalArgumentException | InputMismatchException bo){
+    addToAnExistingPage();
+}
  }
 
 public static void deletePage() throws DiaryPageExeption {
+    try {
         print("please enter date of the Page you wanna delete");
         String date = inputS();
         print("please enter month of the Page you wanna delete");
         String month = inputS();
         diary.deletePageByDate(date, month);
         print("delete successful");
-    while (true) {
-        print("do you want add to the existing gist?\n\n" +
-                "press 1 to go back to the main menu \n" +
-                "press 2 to Edith the Existing Diary Page ");
-        int respond = input();
-        if (respond > 2){
-            continue;
-        }else {
-            switch (respond) {
-                case 1 -> diplayMenu();
-                case 2 -> addToAnExistingPage();
+
+
+            while (true) {
+                print("do you want add to the existing gist?\n\n" +
+                        "press 1 to go back to the main menu \n" +
+                        "press 2 to Edith the Existing Diary Page ");
+                int respond = input();
+                if (respond > 2) {
+                    continue;
+                } else {
+                    switch (respond) {
+                        case 1 -> diplayMenu();
+                        case 2 -> addToAnExistingPage();
+                    }
+                }
             }
+        }catch (DiaryPageExeption | IllegalArgumentException | InputMismatchException to){
+            deletePage();
         }
-    }
 
    }
 
